bc2190b9a53578ad60ec314e21fcdc1d316747e2
Guil Hernandez <g.hernandez07@gmail.com>
2017-01-05 14:51:23 -0500

    syntax edits

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import Gif from './Gif';
 import NoGifs from './NoGifs';
 
-class GifList extends Component {
+export default class GifList extends Component {
   render() {
     const results = this.props.data;
     let gifs;
     
     if(results.length > 0) {
-      gifs = results.map( gif => <Gif img={gif.images.fixed_height.url} key={gif.id}/>) ;
+      gifs = results.map( 
+        gif => <Gif img={gif.images.fixed_height.url} 
+                    key={gif.id}/>);
     } else {
       return <NoGifs />
     }
@@ -22,5 +24,3 @@ class GifList extends Component {
     );
   }
 }
-
-export default GifList;
