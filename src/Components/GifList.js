e97134cde46e12b45b0ce4291b3884ca2939a60d
Guil Hernandez <g.hernandez07@gmail.com>
2017-01-13 11:49:57 -0500

    update function syntax

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Gif from './Gif';
 import NoGifs from './NoGifs';
 
-export default class GifList extends Component {
-  render() {
-    const results = this.props.data;
-    let gifs;   
-    if(results.length > 0) {
-      gifs = results.map(gif => <Gif img={gif.images.fixed_height.url} key={gif.id}/>);
-    } else {
-      gifs = <NoGifs />
-    }
-    return(
-      <ul className="gif-list">
-        {gifs}
-      </ul> 
-    );
+const GifList = props => {  
+    
+  const results = props.data;
+  let gifs;   
+  if(results.length > 0) {
+    gifs = results.map(gif => <Gif img={gif.images.fixed_height.url} key={gif.id}/>);
+  } else {
+    gifs = <NoGifs />
   }
+  
+  return(
+    <ul className="gif-list">
+      {gifs}
+    </ul> 
+  );
 }
+
+export default GifList;
